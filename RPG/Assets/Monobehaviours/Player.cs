a8b58d55602283427b5c6eaf82cb760c4c0162da
Nain415 <78244259+Nain415@users.noreply.github.com>
2022-05-30 17:06:11 -0700

    initial commit

---
+++ RPG/Assets/Monobehaviours/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+
+void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+    	Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+            if (hitObject != null)
+            {
+                print("Hit: " + hitObject.objectName);
+// 1
+                switch (hitObject.itemType)
+                {
+// 2
+                    case Item.ItemType.COIN:
+                        break;
+// 3
+                    case Item.ItemType.HEALTH:
+                        AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
+                        break;
+                }
+                collision.gameObject.SetActive(false);
+            }
+        }
+    }
+// 4
+    public void AdjustHitPoints(int amount)
+    {
+// 5
+        hitPoints = hitPoints + amount;
+        print("Adjusted hitpoints by: " + amount + ". Newvalue: " + hitPoints);
+    }
+}
+
